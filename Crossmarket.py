629678ada9f796cf53f3b37159ea667876341cff
Darknight47 <farhadgunners@gmail.com>
2024-05-12 20:27:39 +0200

    deleted one file

--- Crossmarket.py
+++
@@ -1,9 +0,0 @@
-import math
-cases = int(input())
-for i in range(cases):
-    n, m = map(int, input().split())
-    half = math.floor(n / 2)
-    horiz = n
-    ans = ((half - 1) * 4) + horiz
-    print(ans)
-    print("-----------")
